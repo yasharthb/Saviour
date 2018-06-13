b827dcf5e8dcac54fc7a346fa21bc2d199dad42b
Yasharth Bajpai <yasharthbajpai.99@gmail.com>
2018-06-13 15:57:43 +0530

    Maps_Test

--- app/src/main/java/com/saviour/yasharth/saviour/MainActivity.java
+++ app/src/main/java/com/saviour/yasharth/saviour/MainActivity.java
@@ -1,5 +1,6 @@
 package com.saviour.yasharth.saviour;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -83,6 +84,7 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
+            startActivity(new Intent(MainActivity.this, MapsActivity.class));
 
         } else if (id == R.id.nav_manage) {
 
